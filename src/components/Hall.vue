4a01c182895420941eac523c1fe738d48d8fae48
guoliang.chen@21chinamall.com <guoliang.chen@21chinamall.com>
2016-12-16 17:52:54 +0800

    自己写个页面，并去除Eslint 检查

---
+++ src/components/Hall.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="great">
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+  export default{
+     name:'great',
+     data(){
+        return {
+            msg: 'hello vue'
+        }
+     }
+  }
+
+
+</script>
