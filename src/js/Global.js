de5d8e159fd7b8a2bb4134a440c10d65f88ce836
chen <38033919@qq.com>
2016-12-18 18:29:00 +0800

    增加网络请求,(网络请求没有调试成功)

---
+++ src/js/Global.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chen on 16/12/18.
+ */
+
+class Global{
+  constructor() {
+    this.myurl = "http://amidgame.cn/attendance/getAllLocation";
+  }
+}
+
+export default Global
--- src/js/RequestNet.js
+++ src/js/RequestNet.js
@@ -1,7 +1,48 @@
+import $ from '../js/jquery.min.js'
+import Global from '../js/Global.js'
+
 class RequestNet {
   constructor() {
     //this.$el = el;
     this.color = "yellow";
+
+    // $.ajax({
+    //   type : "POST",  //提交方式
+    //   url : "http://www.runoob.com/",//路径
+    //   data : {
+    //     "org.id" : "${org.id}"
+    //   },//数据，这里使用的是Json格式进行传输
+    //   success : function(result) {//返回数据根据结果进行相应的处理
+    //     if ( result.success ) {
+    //       alert("success");
+    //     } else {
+    //       alert("failed");
+    //     }
+    //   }
+    // });
+
+    $.ajax({
+      type : "GET",  //提交方式
+      url : new Global().myurl,//路径
+      'dataType': 'jsonp',
+      crossDomain : true,
+      xhrFields: {
+        withCredentials: true
+      },
+      headers: {
+        "Accept" : "application/json; charset=utf-8",
+        "Content-Type": "application/javascript; charset=utf-8",
+        "Access-Control-Allow-Origin" : "*"
+      }
+      ,
+      success : function(result) {//返回数据根据结果进行相应的处理
+        if ( result.success ) {
+          alert("success");
+        } else {
+          alert("failed");
+        }
+      }
+    });
   }
 }
 
