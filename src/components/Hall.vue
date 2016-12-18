60b75e46c22f08f7c5c2085d5119cff8f5380190
chen <38033919@qq.com>
2016-12-18 19:26:03 +0800

    html 3等分

--- src/components/Hall.vue
+++ src/components/Hall.vue
@@ -2,8 +2,48 @@
   <div class="great">
     <div>{{ msg }}</div>
     <button v-on:click="addCount()">test</button>
+
+    <div id="left" class="left widhei">1</div>
+    <div id="right" class="right widhei">2</div>
+    <div id="down" class="down widhei">3</div>
+
+    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+        <td style="width:33.3%;"> <div style="background:yellow; height:100px;"></div></td>
+        <td style="width:33.3%;"> <div style="background:red; height:100px;"></div></td>
+        <td style="width:33.4%;"> <div style="background:blue; height:100px;"></div></td>
+      </tr>
+
+    </table>
   </div>
 </template>
+
+<!--<style type="text/css">-->
+  <!--body {color: #000;background: #fff;margin: 0;padding: 0;}-->
+ <!--.left{ background:#606060;}-->
+ <!--.right{ background:#123456; left:70px;top:0px;}-->
+ <!--.down{background:#321321;left:100px;top:-40px;}-->
+ <!--.widhei{width:40px;height:40px}-->
+<!--</style>-->
+
+<!--<style type="text/css">-->
+  <!--body {color: #000;background: #fff;margin: 0;padding: 0;}-->
+ <!--.left{ background:#606060;}-->
+ <!--.right{ background:#123456; left:0px;top:-40px;position:relative;}-->
+ <!--.down{background:#321321;left:0px;top:-80px;position:relative;}-->
+ <!--.widhei{width:40px;height:40px}-->
+<!--</style>-->
+
+<style type="text/css">
+  body {color: #000;background: #fff;margin: 0;padding: 0;}
+ .left{ background:#606060;}
+.right{ background:#123456;top:30px;float:left;}
+.down{background:#321321;float:left;top:0;position:absolute}
+.widhei{width:40px;height:40px}
+</style>
+
+
+
 
 <script>
   import RequestNet from '../js/RequestNet'
